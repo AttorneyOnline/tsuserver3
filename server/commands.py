f966dffe6345712c5179b6f587f6901bcc8fa7e1
ghostfeesh <codeviscerate@gmail.com>
2017-12-22 22:48:42 +0800

    change the url (#19)
    it was never fixed :thinking:

--- server/commands.py
+++ server/commands.py
@@ -184,7 +184,7 @@ def ooc_cmd_pos(client, arg):
 def ooc_cmd_help(client, arg):
     if len(arg) != 0:
         raise ArgumentError('This command has no arguments.')
-    help_url = 'https://github.com/AttorneyOnlineVidya/tsuserver3'
+    help_url = 'https://github.com/AttorneyOnline/tsuserver3/blob/master/README.md'
     help_msg = 'Available commands, source code and issues can be found here: {}'.format(help_url)
     client.send_host_message(help_msg)
         
