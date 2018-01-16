0a4bbd1d7ac70d343ba1822920c4e9722c716c98
Mariomagistr <vityamoshennik@gmail.com>
2018-01-16 13:40:04 +0300

    done
    ban-thing is fixed now

--- server/commands.py
+++ server/commands.py
@@ -203,20 +203,17 @@ def ooc_cmd_kick(client, arg):
         client.send_host_message("No targets found.")
         
 def ooc_cmd_ban(client, arg):
-    print(arg, arg.strip())
-    ipid = int(arg.strip())
-    print(ipid)
     if not client.is_mod:
         raise ClientError('You must be authorized to do that.')
     try:
-        print(15)
+        ipid = int(arg.strip())
+    except:
+        raise ClientError('You must specify ipid')
+    try:
         client.server.ban_manager.add_ban(ipid)
-        print(16)
     except ServerError:
         raise
-    priny('huh')
     if ipid != None:
-        print(12)
         targets = client.server.client_manager.get_targets(client, TargetType.IPID, ipid, False)
         if targets:
             for c in targets:
