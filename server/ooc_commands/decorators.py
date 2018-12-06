9c6655a22feb43354cfd8f7512e9f74f6ce08aa3
argoneus <argoneuscze@gmail.com>
2018-12-06 14:49:24 +0100

    implemented decorators mod_only and no_args

---
+++ server/ooc_commands/decorators.py
@@ -0,0 +1,49 @@
+# tsuserver3, an Attorney Online server
+#
+# Copyright (C) 2018 argoneus <argoneuscze@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+These decorators serve to avoid code repetition in common OOC command checks.
+
+Note the order of the decorators matters, typically the message to be sent
+to be user will be the outermost failing decorator.
+"""
+
+import functools
+
+from server.exceptions import ClientError, ArgumentError
+
+
+def mod_only(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        client = args[0]
+        if not client.is_mod:
+            raise ClientError('You must be logged in as a moderator to do that.')
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def no_args(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        arg = args[1]
+        if len(arg) != 0:
+            raise ArgumentError('This command takes no arguments.')
+        return f(*args, **kwargs)
+
+    return wrapper
