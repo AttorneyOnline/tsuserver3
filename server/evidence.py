228ead70f0776b9b9511543c6cd2bb9d6bb623f7
Alex Noir <Varsash@Gmail.com>
2019-03-19 03:04:38 +0300

    [to be reverted] broken hiddenCM editing system by players when it's not rp-strict. CURRENT ISSUES: Indexes are completely confused. Local, actual indexes mismatch. Hell breaks loose.

--- server/evidence.py
+++ server/evidence.py
@@ -44,44 +44,61 @@ class EvidenceList:
 
     def __init__(self):
         self.evidences = []
-        self.poses = {'def': ['def', 'hld'],
-                      'pro': ['pro', 'hlp'],
-                      'wit': ['wit', 'sea'],
-                      'sea': ['sea', 'wit'],
-                      'hlp': ['hlp', 'pro'],
-                      'hld': ['hld', 'def'],
-                      'jud': ['jud', 'jur'],
-                      'jur': ['jur', 'jud'],
+        self.poses = {'def': ['def'],
+                      'pro': ['pro'],
+                      'wit': ['wit'],
+                      'sea': ['sea'],
+                      'hlp': ['hlp'],
+                      'hld': ['hld'],
+                      'jud': ['jud'],
+                      'jur': ['jur'],
+                      'defense': ['def', 'hld'],
+                      'prosecution': ['pro', 'hlp'],
+                      'benches': ['def', 'hld', 'pro', 'hlp'],
+                      'witness': ['wit', 'sea'],
+                      'judge': ['jud', 'jur'],
                       'all': ['hlp', 'hld', 'wit', 'jud', 'pro', 'def', 'jur', 'sea', ''],
                       'pos': []}
 
-    def login(self, client):
+    def can_see(self, evi, pos):  # used with hiddenCM ebidense
+        for p in evi.pos.strip(' ').split(','):
+            poslist = self.poses[p]
+            if pos in poslist:
+                return True
+        return False
+
+    def login(self, client, strict=True):
         if client.area.evidence_mod == 'FFA':
-            pass
+            return True
         if client.area.evidence_mod == 'Mods':
-            if not client.is_mod:
-                return False
+            if client.is_mod:
+                return True
         if client.area.evidence_mod == 'CM':
-            if not client.is_cm and not client.is_mod:
-                return False
+            if client.is_cm or client.is_mod:
+                return True
         if client.area.evidence_mod == 'HiddenCM':
-            if not client.is_cm and not client.is_mod:
-                return False
-        return True
+            if client.is_cm or client.is_mod or not strict:
+                return True
+        return False
 
     def correct_format(self, client, desc):
         if client.area.evidence_mod != 'HiddenCM':
             return True
         else:
-            # correct format: <owner = pos>\ndesc
-            if desc[:9] == '<owner = ' and desc[9:12] in self.poses and desc[12:14] == '>\n':
+            # correct format: <owner=pos,pos,pos>\ndesc
+            lines = desc.split('\n')
+            cmd = lines[0].strip(' ') #remove all whitespace
+            if cmd[:7] == '<owner=' and cmd.endswith('>'):
                 return True
             return False
 
     def add_evidence(self, client, name, description, image, pos='all'):
-        if self.login(client):
+        if self.login(client, client.hub.status.lower().startswith('rp-strict')):
             if client.area.evidence_mod == 'HiddenCM':
-                pos = 'pos'
+                if self.login(client): #This is a CM/mod, so hide the evidence.
+                    pos = 'pos'
+                else: #This is a normal player. Add the evidence at their pos.
+                    pos = client.pos
             if len(self.evidences) >= self.limit:
                 client.send_host_message('You can\'t have more than {} evidence items at a time.'.format(self.limit))
             else:
@@ -98,9 +115,9 @@ class EvidenceList:
             if client.area.evidence_mod == 'HiddenCM' and self.login(client):
                 nums_list.append(i + 1)
                 evi = self.evidences[i]
-                evi_list.append(self.Evidence(evi.name, '<owner = {}>\n{}'.format(evi.pos, evi.desc), evi.image,
+                evi_list.append(self.Evidence(evi.name, '<owner={}>\n{}'.format(evi.pos, evi.desc), evi.image,
                                               evi.pos).to_string())
-            elif client.pos in self.poses[self.evidences[i].pos]:
+            elif self.can_see(self.evidences[i], client.pos):
                 nums_list.append(i + 1)
                 evi_list.append(self.evidences[i].to_string())
         return nums_list, evi_list
@@ -111,15 +128,49 @@ class EvidenceList:
             self.evidences.append(self.Evidence(name, description, image, pos))
 
     def del_evidence(self, client, id):
-        if self.login(client):
-            self.evidences.pop(id)
+        if self.login(client, client.hub.status.lower().startswith('rp-strict')):
+            if not self.login(client) and client.area.evidence_mod == 'HiddenCM':
+                # The true index in the evidence list we're trying to modify.
+                true_indexes = {}
+                vis = -1
+                for i in range(len(self.evidences)):
+                    # Only care about the visible indexes to us.
+                    if self.can_see(self.evidences[i], client.pos):
+                        vis += 1
+                        # Associate the "visible" index with the true index.
+                        true_indexes[vis] = i
+                print(id, true_indexes[id], true_indexes)
+                # delet the ACTUAL evidence we're trying to bamboozle.
+                self.evidences.pop(true_indexes[id])
+                print("Lax login for evidence deleting. Attacking {} locally ({} true)".format(
+                    id, true_indexes[id]))
+            else:
+                print("Strict login for evidence deleting. Attacking {}".format(id))
+                self.evidences.pop(id)
 
     def edit_evidence(self, client, id, arg):
-        if self.login(client):
-            if client.area.evidence_mod == 'HiddenCM' and self.correct_format(client, arg[1]):
-                self.evidences[id] = self.Evidence(arg[0], arg[1][14:], arg[2], arg[1][9:12])
-                return
+        if self.login(client, client.hub.status.lower().startswith('rp-strict')):
             if client.area.evidence_mod == 'HiddenCM':
-                client.send_host_message('You entered a wrong pos.')
-                return
-            self.evidences[id] = self.Evidence(arg[0], arg[1], arg[2], arg[3])
+                if self.login(client): #strict login
+                    print("Strict login for evidence editing. Attacking {}".format(id))
+                    if self.correct_format(client, arg[1]):
+                        lines = arg[1].split('\n')
+                        cmd = lines[0].strip(' ')  # remove all whitespace
+                        poses = cmd[7:-1]
+                        self.evidences[id] = self.Evidence(arg[0], '\n'.join(lines[1:]), arg[2], poses)
+                    else:
+                        client.send_host_message('You entered a bad pos.')
+                        return
+                else: #lax login - time to do some hacks
+                    true_indexes = {} #The true index in the evidence list we're trying to modify.
+                    vis = -1
+                    for i in range(len(self.evidences)):
+                        if self.can_see(self.evidences[i], client.pos): #Only care about the visible indexes to us.
+                            vis += 1
+                            true_indexes[vis] = i #Associate the "visible" index with the true index.
+                    self.evidences[true_indexes[id]] = self.Evidence(
+                        arg[0], arg[1], arg[2], self.evidences[true_indexes[id]].pos)
+                    print("Lax login for evidence editing. Attacking {} locally ({} true)".format(
+                        id, true_indexes[id]))
+            else:
+                self.evidences[id] = self.Evidence(arg[0], arg[1], arg[2], arg[3])
