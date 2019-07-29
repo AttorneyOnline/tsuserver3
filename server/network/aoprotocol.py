08f7b22c0e5d4895d39afd4e36573af9b6a2b3d2
oldmud0 <oldmud0@users.noreply.github.com>
2019-07-28 22:40:56 -0500

    Use Unicode escape sequences for Zalgo glyphs

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -56,8 +56,9 @@ class AOProtocol(asyncio.Protocol):
         Turns any string into a de-zalgo'd version, with a tolerance to allow for special language characters.
         """
         print(self.server.zalgo_tolerance)
-        banned = b"\\u0300-\\u036f\\u1ab0-\\u1aff\\u1dc0-\\u1dff\\u20d0-\\u20ff\\ufe20-\\ufe2f;"
-        filtered = re.sub('([' + banned.decode('utf-8') + ']{' + re.escape(str(self.server.zalgo_tolerance)) + ",})",'',input)
+        filtered = re.sub('([\u0300\u036f\u1ab0\u1aff\u1dc0\u1dff\u20d0\u20ff\ufe20\ufe2f]' +
+                          '{' + re.escape(str(self.server.zalgo_tolerance)) + ',})',
+                          '', input)
         return filtered
 
     def data_received(self, data):
