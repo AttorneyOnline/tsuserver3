cf6ee5ab5f15830e6e23a08b3fefa78cb80026f7
sD <stoned@derpymail.org>
2019-11-21 12:32:50 +0100

    add python version check

--- start_server.py
+++ start_server.py
@@ -20,6 +20,12 @@
 
 # Install dependencies in case one is missing
 def check_deps():
+    py_version = sys.version_info
+    if py_version.major < 3 or (py_version.major < 3 and py_version.minor < 7):
+        print("tsuserver3 requires at least Python 3.7! Your version: {}.{}"
+            .format(py_version.major, py_version.minor))
+        sys.exit(1)
+
     try:
         import arrow
     except ModuleNotFoundError:
