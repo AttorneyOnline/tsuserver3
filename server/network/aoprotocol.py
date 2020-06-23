3840703940964b7c78b6a6560ebf438649127c78
Crystalwarrior <varsash@gmail.com>
2020-06-23 23:51:11 +0300

    make the protocols whole
    - aoprotocol.py changes:
    Implement the 2.8 netvalidation monstrosity
    Move showname change check to a reasonable place to minimize copypasted code
    Implement default values for everything that's not supported by any given validation check
    Do some tabulation memery on the if check in the pairing finder
    Implement pair_order check to determine who goes in front and who goes in back
    Remove offset_pair = 0 setting so that pairing offset can be used withoutut a need for the pairing partner (It's a feature!)
    Make sure the messages contain the extra packet information introduced to the ic chat
    
    - area_manager.py
    Make sure that legacy music is properly recognized as clientside loopable due to length: not being 0
    
    (cherry picked from commit 8110fc0b02c65aeabc9143ab372b88d0a37d2585)

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -382,7 +382,8 @@ class AOProtocol(asyncio.Protocol):
                                  self.ArgType.INT, self.ArgType.INT,            # anim_type, cid
                                  self.ArgType.INT, self.ArgType.INT_OR_STR,     # sfx_delay, button
                                  self.ArgType.INT, self.ArgType.INT,            # evidence, flip
-                                 self.ArgType.INT, self.ArgType.INT):           # ding, color
+                                 self.ArgType.INT, self.ArgType.INT,            # ding, color
+            ):
             # Pre-2.6 validation monstrosity.
             msg_type, pre, folder, anim, text, pos, sfx, anim_type, cid, sfx_delay, button, evidence, flip, ding, color = args
         elif self.validate_net_cmd(
@@ -392,15 +393,33 @@ class AOProtocol(asyncio.Protocol):
                 self.ArgType.INT, self.ArgType.INT, self.ArgType.INT_OR_STR,    # cid, sfx_delay, button
                 self.ArgType.INT, self.ArgType.INT, self.ArgType.INT,           # evidence, flip, ding
                 self.ArgType.INT, self.ArgType.STR_OR_EMPTY, self.ArgType.INT,  # color, showname, charid_pair
-                self.ArgType.INT, self.ArgType.INT):                            # offset_pair, nonint_pre
-            # 2.6+ validation monstrosity.
+                self.ArgType.INT, self.ArgType.INT,                             # offset_pair, nonint_pre
+            ):
+            # 2.6 validation monstrosity.
             msg_type, pre, folder, anim, text, pos, sfx, anim_type, cid, sfx_delay, button, evidence, flip, ding, color, showname, charid_pair, offset_pair, nonint_pre = args
-            if len(showname
-                   ) > 0 and not self.client.area.showname_changes_allowed:
-                self.client.send_ooc(
-                    "Showname changes are forbidden in this area!")
-                return
+        elif self.validate_net_cmd(
+                args, self.ArgType.STR, self.ArgType.STR_OR_EMPTY,              # msg_type, pre
+                self.ArgType.STR, self.ArgType.STR, self.ArgType.STR,           # folder, anim, text
+                self.ArgType.STR, self.ArgType.STR, self.ArgType.INT,           # pos, sfx, anim_type
+                self.ArgType.INT, self.ArgType.INT, self.ArgType.INT_OR_STR,    # cid, sfx_delay, button
+                self.ArgType.INT, self.ArgType.INT, self.ArgType.INT,           # evidence, flip, ding
+                self.ArgType.INT, self.ArgType.STR_OR_EMPTY, self.ArgType.STR,  # color, showname, charid_pair
+                self.ArgType.INT, self.ArgType.INT, self.ArgType.STR,           # offset_pair, nonint_pre, sfx_looping
+                self.ArgType.INT, self.ArgType.STR, self.ArgType.STR,           # screenshake, frames_shake, frames_realization
+                self.ArgType.STR, self.ArgType.INT, self.ArgType.STR,           # frames_sfx, additive, effect
+            ):
+            # 2.8 validation monstrosity. (rip 2.7)
+            msg_type, pre, folder, anim, text, pos, sfx, anim_type, cid, sfx_delay, button, evidence, flip, ding, color, showname, charid_pair, offset_pair, nonint_pre, sfx_looping, screenshake, frames_shake, frames_realization, frames_sfx, additive, effect = args
+            pair_args = charid_pair.split("^")
+            charid_pair = int(pair_args[0])
+            if (len(pair_args) > 1):
+                pair_order = pair_args[1]
         else:
+            return
+
+        if len(showname) > 0 and not self.client.area.showname_changes_allowed:
+            self.client.send_ooc(
+                "Showname changes are forbidden in this area!")
             return
         if self.client.area.is_iniswap(self.client, pre, anim,
                 folder, sfx):
@@ -534,17 +553,21 @@ class AOProtocol(asyncio.Protocol):
         confirmed = False
         if charid_pair > -1:
             for target in self.client.area.clients:
-                if target.char_id == self.client.charid_pair and target.charid_pair == self.client.char_id and target != self.client and target.pos == self.client.pos:
+                if not confirmed and target.char_id == self.client.charid_pair and
+                          target.charid_pair == self.client.char_id and target != self.client and
+                          target.pos == self.client.pos:
                     confirmed = True
                     other_offset = target.offset_pair
                     other_emote = target.last_sprite
                     other_flip = target.flip
                     other_folder = target.claimed_folder
+                    if (pair_order != ""):
+                        charid_pair = "{}^{}".format(charid_pair, pair_order)
                     break
 
         if not confirmed:
             charid_pair = -1
-            offset_pair = 0
+
         if self.client in self.client.area.afkers:
             self.client.server.client_manager.toggle_afk(self.client)
         self.client.area.send_command('MS', msg_type, pre, folder, anim, msg,
@@ -552,20 +575,27 @@ class AOProtocol(asyncio.Protocol):
                                       button, self.client.evi_list[evidence],
                                       flip, ding, color, showname, charid_pair,
                                       other_folder, other_emote, offset_pair,
-                                      other_offset, other_flip, nonint_pre)
+                                      other_offset, other_flip, nonint_pre,
+                                      sfx_looping, screenshake, frames_shake,
+                                      frames_realization, frames_sfx,
+                                      additive, effect)
 
         self.client.area.send_owner_command(
             'MS', msg_type, pre, folder, anim,
             '[' + self.client.area.abbreviation + ']' + msg, pos, sfx,
             anim_type, cid, sfx_delay, button, self.client.evi_list[evidence],
             flip, ding, color, showname, charid_pair, other_folder,
-            other_emote, offset_pair, other_offset, other_flip, nonint_pre)
+            other_emote, offset_pair, other_offset, other_flip, nonint_pre,
+            sfx_looping, screenshake, frames_shake, frames_realization,
+            frames_sfx, additive, effect)
 
         self.server.area_manager.send_remote_command(
             target_area, 'MS', msg_type, pre, folder, anim, msg, pos, sfx,
             anim_type, cid, sfx_delay, button, self.client.evi_list[evidence],
             flip, ding, color, showname, charid_pair, other_folder,
-            other_emote, offset_pair, other_offset, other_flip, nonint_pre)
+            other_emote, offset_pair, other_offset, other_flip, nonint_pre,
+            sfx_looping, screenshake, frames_shake, frames_realization,
+            frames_sfx, additive, effect)
 
         self.client.area.set_next_msg_delay(len(msg))
         database.log_ic(self.client, self.client.area, showname, msg)
