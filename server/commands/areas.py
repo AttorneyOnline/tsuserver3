07ced041c26edb94a9a20dbd17a939302ceadbee
Crystalwarrior <varsash@gmail.com>
2020-07-24 21:00:03 +0300

    Fix /lock and /unlock unexpected behavior Dictate that !1 is a link and 1 is an area id Mention in /link_lock and /link_unlock that the link must exist previously.

--- server/commands/areas.py
+++ server/commands/areas.py
@@ -261,38 +261,47 @@ def ooc_cmd_area_unlock(client, arg):
 def ooc_cmd_lock(client, arg):
     """
     Context-sensitive function to lock area(s) and/or area link(s).
-    Usage: /lock - lock current area. /lock [id] - lock target area. /lock 1-5 - lock the link from area 1 to area 5.
+    Usage: /lock - lock current area. /lock [id] - lock target area. /lock !5 - lock the link from current area to area 5.
     Multiple targets may be passed.
     """
     args = arg.split()
-    areas = args
+    areas = args.copy()
+    links = []
     for a in args:
-        link = a.split('-')
-        if len(link) <= 1:
+        if not a.startswith('!'):
             continue
-        ooc_cmd_link_lock(client, ' '.join([link[0], link[1]]))
         areas.remove(a)
-    areas = ' '.join(areas)
-    ooc_cmd_area_lock(client, areas)
+        links.append(a[1:])
+    if len(areas) > 0:
+        areas = ' '.join(areas)
+        ooc_cmd_area_lock(client, areas)
+    if len(links) > 0:
+        links = ' '.join(links)
+        print(links)
+        ooc_cmd_link_lock(client, links)
 
 
 @mod_only()
 def ooc_cmd_unlock(client, arg):
     """
     Context-sensitive function to unlock area(s) and/or area link(s).
-    Usage: /unlock - unlock current area. /unlock [id] - lock target area. /unlock 1-5 - lock the link from area 1 to area 5.
+    Usage: /unlock - unlock current area. /unlock [id] - unlock target area. /unlock !5 - unlock the link from current area to area 5.
     Multiple targets may be passed.
     """
     args = arg.split()
-    areas = args
+    areas = args.copy()
+    links = []
     for a in args:
-        link = a.split('-')
-        if len(link) <= 1:
+        if not a.startswith('!'):
             continue
-        ooc_cmd_link_unlock(client, ' '.join([link[0], link[1]]))
         areas.remove(a)
-    areas = ' '.join(areas)
-    ooc_cmd_area_unlock(client, areas)
+        links.append(a[1:])
+    if len(areas) > 0:
+        areas = ' '.join(areas)
+        ooc_cmd_area_unlock(client, areas)
+    if len(links) > 0:
+        links = ' '.join(links)
+        ooc_cmd_link_unlock(client, links)
 
 
 @mod_only(area_owners=True)
@@ -699,13 +708,12 @@ def ooc_cmd_link_lock(client, arg):
                 target_id = client.server.area_manager.get_area_by_abbreviation(aid).id
             except:
                 target_id = int(aid)
-
             client.area.links[str(target_id)]["locked"] = True
             links.append(target_id)
         links = ', '.join(str(l) for l in links)
         client.send_ooc(f'Area {client.area.name} links {links} locked.')
     except (ValueError, KeyError):
-        raise ArgumentError('Area ID must be a number or abbreviation.')
+        raise ArgumentError('Area ID must be a number or abbreviation and the link must exist.')
     except (AreaError, ClientError):
         raise
 
@@ -732,7 +740,7 @@ def ooc_cmd_link_unlock(client, arg):
         links = ', '.join(str(l) for l in links)
         client.send_ooc(f'Area {client.area.name} links {links} unlocked.')
     except (ValueError, KeyError):
-        raise ArgumentError('Area ID must be a number or abbreviation.')
+        raise ArgumentError('Area ID must be a number or abbreviation and the link must exist.')
     except (AreaError, ClientError):
         raise
 
