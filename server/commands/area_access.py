cfd87ee2b4e3f1325edac62b3f0ee441f7c5499f
Crystalwarrior <varsash@gmail.com>
2020-07-30 17:54:04 +0300

    rename /area_links to /area_access and move all locking/unlocking commands into it

--- server/commands/area_access.py
+++ server/commands/area_access.py
@@ -2,6 +2,11 @@ from server.exceptions import ClientError, ArgumentError, AreaError
 from . import mod_only
 
 __all__ = [
+    'ooc_cmd_area_lock',
+    'ooc_cmd_area_spectate',
+    'ooc_cmd_area_unlock',
+    'ooc_cmd_lock',
+    'ooc_cmd_unlock',
     'ooc_cmd_link',
     'ooc_cmd_unlink',
     'ooc_cmd_links',
@@ -16,6 +21,182 @@ __all__ = [
     'ooc_cmd_link_evidence',
     'ooc_cmd_unlink_evidence',
 ]
+
+
+def ooc_cmd_area_lock(client, arg):
+    """
+    Prevent users from joining the current area.
+    Usage: /area_lock
+    """
+    args = arg.split()
+    if len(args) == 0:
+        args = [client.area.id]
+
+    try:
+        area_list = []
+        for aid in args:
+            try:
+                target_id = client.area.area_manager.get_area_by_abbreviation(aid).id
+            except:
+                target_id = int(aid)
+            area = client.area.area_manager.get_area_by_id(target_id)
+
+            if not client.is_mod:
+                if not area.locking_allowed:
+                    client.send_ooc(f'Area locking is disabled in area {area.name}.')
+                    continue
+                if not client in area.owners:
+                    if not str(target_id) in client.keys:
+                        client.send_ooc(f'You don\'t have the keys to this {area.name}.')
+                        continue
+                    if not client.can_access_area(area):
+                        client.send_ooc(f'You have the keys to {area.name} but it is not accessible from your area.')
+                        continue
+            if area.is_locked == client.area.Locked.LOCKED:
+                client.send_ooc(f'Area {area.name} is already locked.')
+                continue
+            area.lock()
+            area_list.append(area.id)
+        if len(area_list) > 0:
+            client.send_ooc(f'Locked areas {area_list}.')
+    except ValueError:
+        raise ArgumentError('Target must be an abbreviation or number.')
+    except (ClientError, AreaError):
+        raise
+
+
+def ooc_cmd_area_spectate(client, arg):
+    """
+    Allow users to join the current area, but only as spectators.
+    Usage: /area_spectate
+    """
+    args = arg.split()
+    if len(args) == 0:
+        args = [client.area.id]
+
+    try:
+        area_list = []
+        for aid in args:
+            try:
+                target_id = client.area.area_manager.get_area_by_abbreviation(aid).id
+            except:
+                target_id = int(aid)
+            area = client.area.area_manager.get_area_by_id(target_id)
+
+            if not client.is_mod:
+                if not area.locking_allowed:
+                    client.send_ooc(f'Area locking is disabled in area {area.name}.')
+                    continue
+                if not client in area.owners:
+                    if not str(target_id) in client.keys:
+                        client.send_ooc(f'You don\'t have the keys to this {area.name}.')
+                        continue
+                    if not client.can_access_area(area):
+                        client.send_ooc(f'You have the keys to {area.name} but it is not accessible from your area.')
+                        continue
+            if area.is_locked == client.area.Locked.SPECTATABLE:
+                client.send_ooc(f'Area {area.name} is already spectatable.')
+                continue
+            area.spectator()
+            area_list.append(area.id)
+        if len(area_list) > 0:
+            client.send_ooc(f'Made areas {area_list} spectatable.')
+    except ValueError:
+        raise ArgumentError('Target must be an abbreviation or number.')
+    except (ClientError, AreaError):
+        raise
+
+
+def ooc_cmd_area_unlock(client, arg):
+    """
+    Allow anyone to freely join the current area.
+    Usage: /area_unlock
+    """
+    args = arg.split()
+    if len(args) == 0:
+        args = [client.area.id]
+
+    try:
+        area_list = []
+        for aid in args:
+            try:
+                target_id = client.area.area_manager.get_area_by_abbreviation(aid).id
+            except:
+                target_id = int(aid)
+            area = client.area.area_manager.get_area_by_id(target_id)
+
+            if not client.is_mod:
+                if not area.locking_allowed:
+                    client.send_ooc(f'Area locking is disabled in area {area.name}.')
+                    continue
+                if not client in area.owners:
+                    if not str(target_id) in client.keys:
+                        client.send_ooc(f'You don\'t have the keys to this {area.name}.')
+                        continue
+                    if not client.can_access_area(area):
+                        client.send_ooc(f'You have the keys to {area.name} but it is not accessible from your area.')
+                        continue
+            if area.is_locked == client.area.Locked.FREE:
+                client.send_ooc(f'Area {area.name} is already unlocked.')
+                continue
+            area.unlock()
+            area_list.append(area.id)
+        if len(area_list) > 0:
+            client.send_ooc(f'Unlocked areas {area_list}.')
+    except ValueError:
+        raise ArgumentError('Target must be an abbreviation or number.')
+    except (ClientError, AreaError):
+        raise
+
+
+def ooc_cmd_lock(client, arg):
+    """
+    Context-sensitive function to lock area(s) and/or area link(s).
+    Usage: /lock - lock current area. /lock [id] - lock target area. /lock !5 - lock the link from current area to area 5.
+    Multiple targets may be passed.
+    """
+    if arg == '':
+        arg = str(client.area.id)
+    args = arg.split()
+    areas = args.copy()
+    links = []
+    for a in args:
+        if not a.startswith('!'):
+            continue
+        areas.remove(a)
+        links.append(a[1:])
+    if len(areas) > 0:
+        areas = ' '.join(areas)
+        ooc_cmd_area_lock(client, areas)
+    if len(links) > 0:
+        links = ' '.join(links)
+        print(links)
+        ooc_cmd_link_lock(client, links)
+
+
+def ooc_cmd_unlock(client, arg):
+    """
+    Context-sensitive function to unlock area(s) and/or area link(s).
+    Usage: /unlock - unlock current area. /unlock [id] - unlock target area. /unlock !5 - unlock the link from current area to area 5.
+    Multiple targets may be passed.
+    """
+    if arg == '':
+        arg = str(client.area.id)
+    args = arg.split()
+    areas = args.copy()
+    links = []
+    for a in args:
+        if not a.startswith('!'):
+            continue
+        areas.remove(a)
+        links.append(a[1:])
+    if len(areas) > 0:
+        areas = ' '.join(areas)
+        ooc_cmd_area_unlock(client, areas)
+    if len(links) > 0:
+        links = ' '.join(links)
+        ooc_cmd_link_unlock(client, links)
+
 
 @mod_only(area_owners=True)
 def ooc_cmd_link(client, arg):
