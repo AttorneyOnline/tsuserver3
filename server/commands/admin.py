f826c801ab84561ef682b8a206a7a95dcef92f90
Crystalwarrior <varsash@gmail.com>
2020-08-03 00:13:05 +0300

    Fix the link in /help 404'ing ...due to missing space at the end of the link making it merge with new line

--- server/commands/admin.py
+++ server/commands/admin.py
@@ -55,7 +55,7 @@ def ooc_cmd_help(client, arg):
         If you don't understand a specific core feature, check the official
         repository for more information:
 
-        https://github.com/Crystalwarrior/KFO-Server
+        https://github.com/Crystalwarrior/KFO-Server 
 
         Available Categories:
         ''')
