a249addae145c41edf79f8633247eda01ee761cf
in1tiate <32779090+in1tiate@users.noreply.github.com>
2020-12-17 11:13:55 -0600

    add validation for desk_mod 4 and 5

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -482,7 +482,7 @@ class AOProtocol(asyncio.Protocol):
                 self.client.send_ooc('You don\'t any areas!')
                 return
             text = ' '.join(part[1:])
-        if msg_type not in ('chat', '0', '1', '2', '3'):
+        if msg_type not in ('chat', '0', '1', '2', '3', '4', '5'):
             return
         if anim_type == 4:
             anim_type = 6
