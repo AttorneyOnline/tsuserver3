79e19d017b1e97f0d6362c98f311d6c3955861a7
in1tiate <32779090+in1tiate@users.noreply.github.com>
2020-12-21 03:32:42 -0600

    don't navigate or create empty testimonies

--- server/area_manager.py
+++ server/area_manager.py
@@ -548,6 +548,9 @@ class AreaManager:
                 client.send_ooc('You don\'t own this area!')
                 return False
             elif self.is_testifying:
+                if not self.testimony.statements:
+                    client.send_ooc('Please add at least one statement before ending your testimony.')
+                    return False
                 self.is_testifying = False
                 self.broadcast_ooc('Recording stopped.')
                 return True
@@ -594,6 +597,9 @@ class AreaManager:
             Navigate the current testimony using the commands >, <, =, and [>|<]<index>.
             Returns False if the navigation was unsuccessful.
             """
+            if not self.testimony.statements:
+                client.send_ooc('Testimony is empty, can\'t navigate!')
+                return False
             if index == None:
                 if command == '=':
                     if self.examine_index == 0:
@@ -617,6 +623,7 @@ class AreaManager:
                     client.send_ooc("That does not look like a valid statement number!")
                     return False
             self.send_command('MS', *self.testimony.statements[self.examine_index])
+            return True
                 
         class JukeboxVote:
             """Represents a single vote cast for the jukebox."""
