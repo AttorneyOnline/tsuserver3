40fbc331878d319b110858d8212b27853825a30a
stonedDiscord <Tukz@gmx.de>
2021-02-23 20:06:52 +0100

    past bans #98

--- migrations/v4.sql
+++ migrations/v4.sql
@@ -32,6 +32,23 @@ ALTER TABLE ic_events_new RENAME TO ic_events;
 
 CREATE INDEX idx_event_ip ON ic_events (event_time, ipid);
 
+-- Remove FOREIGN KEY constraint from ip_bans
+CREATE TABLE ip_bans_new(
+	ipid TEXT PRIMARY KEY,
+	ban_id INTEGER NOT NULL,
+	FOREIGN KEY (ban_id) REFERENCES bans(ban_id)
+		ON DELETE CASCADE
+);
+INSERT INTO ip_bans_new SELECT * FROM ip_bans;
+DROP TABLE ip_bans;
+ALTER TABLE ip_bans_new RENAME TO ip_bans;
+
+-- Add unbanned column
+ALTER TABLE bans ADD unbanned INTEGER DEFAULT 0;
+
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys = OFF;
+
 VACUUM;
 
 PRAGMA user_version = 4;
