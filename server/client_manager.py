e46bb3280c11cd35e3200732d732ab05b44db131
stonedDiscord <Tukz@gmx.de>
2021-02-23 19:14:49 +0100

    missing bracket

--- server/client_manager.py
+++ server/client_manager.py
@@ -141,7 +141,7 @@ class ClientManager:
             self.send_ooc(f'=== MOTD ===\r\n{motd}\r\n=============')
             datenow = datetime.now()
             if self.server.config['special_message_date'] in str(datenow):
-                self.send_ooc(self.server.config['special_message_title'] + f'\r\n' +self.server.config['special_message_content'] + f'\r\n============='
+                self.send_ooc(self.server.config['special_message_title'] + f'\r\n' +self.server.config['special_message_content'] + f'\r\n=============')
 
         def send_player_count(self):
             """
