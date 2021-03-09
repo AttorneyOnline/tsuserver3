4159aa7d3703f78e660015b3541139059a009912
in1tiate <radwoodward@vikings.grayson.edu>
2021-03-09 14:46:42 -0600

    update FL

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -266,7 +266,7 @@ class AOProtocol(asyncio.Protocol):
                                  'deskmod', 'evidence', 'modcall_reason',
                                  'cccc_ic_support', 'arup', 'casing_alerts',
                                  'prezoom', 'looping_sfx', 'additive', 'effects',
-                                 'y_offset', 'expanded_desk_mods')
+                                 'y_offset', 'expanded_desk_mods', 'auth_packet')
 
     def net_cmd_ch(self, _):
         """Reset the client drop timeout (keepalive).
