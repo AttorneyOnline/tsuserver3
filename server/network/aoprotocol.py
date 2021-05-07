cc885bd7cc914ded9da3d898467c1d13f48a6965
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2021-05-07 13:55:20 -0400

    Support for Asset packet

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -257,6 +257,12 @@ class AOProtocol(asyncio.Protocol):
                                  'cccc_ic_support', 'arup', 'casing_alerts',
                                  'prezoom', 'looping_sfx', 'additive', 'effects',
                                  'y_offset', 'expanded_desk_mods')
+        
+        # Send Asset packet if asset_url is defined
+        if self.server.config['asset_url'] != '':
+            # Convert Spaces for browsers
+            cleaned_url: str = self.server.config['asset_url'].replace(' ', '<percent>20')
+            self.client.send_command('ASS', cleaned_url)
 
     def net_cmd_ch(self, _):
         """Reset the client drop timeout (keepalive).
