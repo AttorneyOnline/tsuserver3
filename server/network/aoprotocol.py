62f6f9f73d44e6bbd190a5ebd6f1ea244a2e441a
stonedDiscord <Tukz@gmx.de>
2021-05-12 07:08:27 +0200

    Add asset packet (#178)
    Co-authored-by: Crystalwarrior <varsash@gmail.com>
    Co-authored-by: oldmud0 <oldmud0@users.noreply.github.com>

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -267,6 +267,9 @@ class AOProtocol(asyncio.Protocol):
                                  'cccc_ic_support', 'arup', 'casing_alerts',
                                  'prezoom', 'looping_sfx', 'additive', 'effects',
                                  'y_offset', 'expanded_desk_mods')
+        # Send Asset packet if asset_url is defined
+        if self.server.config['asset_url'] != '':
+            self.client.send_command('ASS', self.server.config['asset_url'])
 
     def net_cmd_ch(self, _):
         """Reset the client drop timeout (keepalive).
