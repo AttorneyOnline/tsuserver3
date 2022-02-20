aaca5ee37ad01b859b2359508cc6f383b23e631f
Alex Noir <Varsash@gmail.com>
2022-02-20 04:18:38 +0300

    Make IC passing messages appear instantly

--- server/client_manager.py
+++ server/client_manager.py
@@ -873,7 +873,7 @@ class ClientManager:
                     if old_area.area_manager == self.area.area_manager:
                         if self.area.area_manager.passing_msg == True:
                             old_area.send_ic(
-                                None, '1', 1, "", "", f'~~}}[º{self.showname}º leaves to º{area.name}º.]', 
+                                None, '1', 1, "", "", f'~~{"}}}"}[º{self.showname}º leaves to º{area.name}º.]', 
                                 "", "", 1, -1, 0, 0, [0], 0, 0, 0, "", -1, "", "", 0, 0, 0, 0, "0", 0, "", "", "", 0, ""
                             )
                         for c in old_area.clients:
@@ -919,7 +919,7 @@ class ClientManager:
                     )
                     if self.area.area_manager.passing_msg == True:
                         self.area.send_ic(
-                            None, '1', 1, "", "", f'~~}}[º{self.showname}º enters from º{old_area.name}º.]', 
+                            None, '1', 1, "", "", f'~~{"}}}"}[º{self.showname}º enters from º{old_area.name}º.]', 
                             "", "", 1, -1, 0, 0, [0], 0, 0, 0, "", -1, "", "", 0, 0, 0, 0, "0", 0, "", "", "", 0, ""
                         )
                 else:
